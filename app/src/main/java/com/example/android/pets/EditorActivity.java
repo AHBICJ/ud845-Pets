92e0378d919bfe6c64c1a32cc0d7f054ef2fac75
AHBICJ <810988732@qq.com>
2019-11-28 01:25:53 +0800

    通过Intent里的Uri修改Editor的标题

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -62,7 +62,13 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
-
+        Uri currentPetUri = getIntent().getData();
+        if (currentPetUri==null){
+            setTitle(getString(R.string.editor_activity_title_new_pet));
+        }
+        else{
+            setTitle(getString(R.string.editor_activity_title_edit_pet));
+        }
         // Find all relevant views that we will need to read user input from
         mNameEditText = (EditText) findViewById(R.id.edit_pet_name);
         mBreedEditText = (EditText) findViewById(R.id.edit_pet_breed);
